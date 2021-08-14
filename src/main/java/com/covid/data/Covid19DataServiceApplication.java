f8e5d1321b939bd9bfcb9a97d1e1c99259734e98
dharmendrajaypuria <jdharmendra0723@gmail.com>
2021-08-14 10:28:11 +0530

    initial commit

---
+++ src/main/java/com/covid/data/Covid19DataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.covid.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Covid19DataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Covid19DataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/covid/data/Covid19DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
